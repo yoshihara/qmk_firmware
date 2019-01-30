3bdbaa3fcbc1896d0452caea65603833d754f047
yoshihara <594134+yoshihara@users.noreply.github.com>
2019-01-31 01:07:38 +0900

    gherkin: remove SYMB layer & renew keymap

--- keyboards/40percentclub/gherkin/keymaps/hyoshihara04/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/hyoshihara04/keymap.c
@@ -1,8 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #define BASE 0
-#define SYMB 1
-#define NUMS 2
+#define NUMS 1
 
 // https://docs.qmk.fm/#/feature_tap_dance
 //Tap Dance Declarations
@@ -21,23 +20,16 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_ortho_3x10(
-    TD(TD_MINS_Q),  KC_W, TD(TD_COMM_DOT), KC_SPC, KC_BSPC, LCMD_T(KC_M), KC_R,    KC_D,    KC_Y,    KC_P,
-    LT(SYMB, KC_A), KC_O, KC_E,            KC_I,   KC_U,    KC_G,         KC_T,    KC_K,    KC_S,    KC_N,
-    LT(NUMS, KC_Z), KC_X, KC_C,            KC_V,   KC_F,    KC_B,         KC_H,    KC_J,    KC_L,    KC_ENT
-  ),
-
-  [SYMB] = LAYOUT_ortho_3x10(
-    _______, _______, _______, _______, _______, _______,    KC_LBRC, KC_SCOLON, S(KC_9), _______,
-    _______, _______, _______, _______, _______, S(KC_SLSH), KC_RBRC, KC_UP,     S(KC_0), KC_LCTL,
-    _______, _______, _______, KC_LSFT, _______, S(KC_1),    KC_LEFT, KC_DOWN,   KC_RGHT, _______
+    KC_MINS,        KC_W,    KC_COMM, KC_SPC, KC_BSPC, KC_M,         KC_R,    KC_D,      KC_Y,    KC_P,
+    KC_A,           KC_O,    KC_E,    KC_I,   KC_U,    KC_G,         KC_T,    KC_K,      KC_S,    KC_N,
+    LT(NUMS, KC_Z), KC_X,    KC_C,    KC_V,   KC_F,    LCMD_T(KC_B), KC_H,    KC_J,      KC_L,    KC_ENT
   ),
 
   [NUMS] = LAYOUT_ortho_3x10(
-    _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,    KC_8,    KC_9,    KC_0,
-    _______, _______, _______, _______, RESET,   _______,    _______, _______, _______, _______
+    KC_Q,           KC_1,    KC_DOT,  KC_SPC, KC_BSPC, S(KC_1),      KC_LBRC, KC_SCOLON, S(KC_9), KC_LSFT,
+    RESET,          KC_2,    KC_3,    KC_4,   KC_5,    S(KC_SLSH),   KC_RBRC, KC_UP,     S(KC_0), KC_LCTL,
+    _______,        KC_6,    KC_7,    KC_8,   KC_9,    KC_0,         KC_LEFT, KC_DOWN,   KC_RGHT, _______
   ),
-
 };
 
 void matrix_init_user(void) {
