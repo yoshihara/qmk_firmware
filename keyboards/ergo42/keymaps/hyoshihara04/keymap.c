52189ead11cfa32709339288aef3c3fdac16f8d6
yoshihara <594134+yoshihara@users.noreply.github.com>
2019-01-31 22:20:41 +0900

    ergo42: renew SYMB (for game)

--- keyboards/ergo42/keymaps/hyoshihara04/keymap.c
+++ keyboards/ergo42/keymaps/hyoshihara04/keymap.c
@@ -28,10 +28,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [SYMB] = LAYOUT( \
-    _______,    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_0,    KC_F7,   KC_F8,      KC_F9,     KC_F10,   KC_F11,  KC_F12,  _______, \
-    _______,    _______, KC_Q,    KC_W,    KC_5,    KC_6,    KC_7,    KC_F1,   KC_F2,      KC_F3,     KC_F4,    KC_F5,   KC_F6,   KC_ENT, \
-    _______,    _______, KC_A,    KC_S,    KC_D,    KC_8,    KC_9,    XXXXXXX, XXXXXXX,    XXXXXXX,   KC_UP,    XXXXXXX, XXXXXXX, KC_RALT, \
-    _______,    _______, _______, KC_SPC,  _______, _______, _______, _______, _______,    KC_LEFT,   KC_DOWN,  KC_RGHT, KC_RGUI, TO(BASE) \
+    _______,    _______, KC_Q,    KC_W,    KC_0,    KC_1,    KC_2,    KC_F1,   KC_F2,      KC_F3,     KC_F4,    KC_F5,   KC_F6,   _______, \
+    _______,    _______, KC_A,    KC_S,    KC_D,    KC_3,    KC_4,    KC_F7,   KC_F8,      KC_F9,     KC_F10,   KC_F11,  KC_F12,  KC_ENT, \
+    _______,    _______, KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    XXXXXXX, XXXXXXX,    XXXXXXX,   KC_UP,    XXXXXXX, XXXXXXX, KC_RALT, \
+    _______,    _______, _______, _______, _______, _______, _______, _______, _______,    KC_LEFT,   KC_DOWN,  KC_RGHT, KC_RGUI, TO(BASE) \
   )
 
 };
