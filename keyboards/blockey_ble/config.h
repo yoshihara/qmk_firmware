369c7f72294b2d8d8e6e71e8419b44f780976a56
yoshihara <594134+yoshihara@users.noreply.github.com>
2019-02-20 14:29:24 +0900

    blockey_ble: Update Keyboard info

--- keyboards/blockey_ble/config.h
+++ keyboards/blockey_ble/config.h
@@ -23,13 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6464
+#define PRODUCT_ID      0x0002
 #define DEVICE_VER      0x0001
 /* in python2: list(u"whatever".encode('utf-16-le')) */
 /*   at most 32 characters or the ugly hack in usb_main.c borks */
-#define MANUFACTURER QMK
-#define PRODUCT BLE Micro Test
-#define DESCRIPTION QMK based keyboard
+#define MANUFACTURER Eucalyn
+#define PRODUCT Blockey BLE
+#define DESCRIPTION Blockey with BLE Micro Pro
 
 /* key matrix size */
 #define MATRIX_ROWS 10
