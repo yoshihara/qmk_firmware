928f54b228c37bcb04c2c25b78c63f4cf895fea2
yoshihara <594134+yoshihara@users.noreply.github.com>
2019-05-05 15:39:55 +0900

    helix: Tweak keymap

--- keyboards/helix/rev2/keymaps/hyoshihara04/keymap.c
+++ keyboards/helix/rev2/keymaps/hyoshihara04/keymap.c
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RBRC, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV, KC_SCST,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP, \
-      ADJUST,  KC_QUOT, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_EQL, KC_MINS,  RAISE,   KC_ENT,  KC_RGUI, KC_LEFT, KC_RGHT, KC_DOWN \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_SCST, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP, \
+      ADJUST,  KC_QUOT, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_LGUI, KC_RSFT, RAISE,   KC_ENT,  KC_RGUI, KC_LEFT, KC_RGHT, KC_DOWN \
       ),
 
   /* Lower
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_EQL,  KC_MINS, KC_QUOT, KC_BSLS, KC_GRV,  KC_LCBR, \
       _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_EQL,  KC_MINS, KC_QUOT, KC_BSLS, KC_GRV,  KC_RCBR, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  AU_ON, \
-      _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, AU_OFF \
+      _______, _______, _______, _______, _______, _______, KC_PLUS, _______, _______, _______, _______, _______, _______, AU_OFF \
       ),
 
   /* Raise
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_PLUS, KC_UNDS, KC_DQUO, KC_PIPE, KC_TILD, KC_LPRN, \
       _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                   KC_PLUS, KC_UNDS, KC_DQUO, KC_PIPE, KC_TILD, KC_RPRN, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, \
-      _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______, XXXXXXX \
+      _______, _______, _______, _______, _______, _______, KC_PLUS, _______, _______, _______, _______, _______, _______, XXXXXXX \
       ),
 
   /* Adjust (Lower + Raise)
